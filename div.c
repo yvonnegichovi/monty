b468634ad1f57d24a13ed4e1d364f10fbfde9359
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-16 12:24:58 -0800

    div task

--- div.c
+++ div.c
@@ -8,22 +8,23 @@
 
 void _div(stack_t **stack, unsigned int line_number)
 {
+	int result;
+	stack_t *temp;
+
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-		free_stack(*stack);
-		fclose(glob.file);
-		free(glob.line);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n == 0)
+
+	temp = *stack;
+	if (temp->n == 0)
 	{
-		fprintf(stderr, "L%u: division by zero\n", line_number);
-		free_stack(*stack);
-		fclose(glob.file);
-		free(glob.line);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->n /= (*stack)->n;
-	_pop(stack, line_number);
+	result = temp->next->n / temp->n;
+	temp->next->n = result;
+	*stack = temp->next;
+	free(temp);
 }
