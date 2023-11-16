3d74da1fc4e1dae935a6272bf5b66575c2acd46b
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-16 12:54:02 -0800

    fixing the pchar code

--- pchar.c
+++ pchar.c
@@ -11,17 +11,11 @@ void _pchar(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		free_stack(*stack);
-		fclose(glob.file);
-		free(glob.line);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-		free_stack(*stack);
-		fclose(glob.file);
-		free(glob.line);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
