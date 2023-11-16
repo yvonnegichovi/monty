fa18ac1ddcb4271163ca6440a2ca363211ed5d58
yvonnegichovi <yvonnegichovi@gmail.com>
2023-11-16 11:59:59 -0800

    fixing swap

--- swap.c
+++ swap.c
@@ -13,6 +13,8 @@ void _swap(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free(glob.line);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 	temp = (*stack)->next;
